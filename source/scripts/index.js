1656eb94f96f48ef461b1f3b97dc15720385f788
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-10-26 00:45:25 +0300

    feat: add custom checkbox and filter range styles

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,24 @@
 /* в этот файл добавляет скрипты*/
+
+// бургер выпадающего меню на версии mobile
+
+// слайдер выбора цены
+const filterSlider = document.querySelector('.filter__slider');
+
+const createSlider = () => {
+  noUiSlider.create(filterSlider, {
+    range: {
+      min: 0,
+      max: 900,
+    },
+    start: [0, 750],
+    step: 1,
+    connect: true,
+    format: {
+      to: (value) => Number(value),
+      from: (value) => Number(value),
+    }
+  });
+};
+
+createSlider();
